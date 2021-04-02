c2f491fc3ece26f48bd04923f1762ff8d02a491e
Eoin Pinaqui <pinaquie@tcd.ie>
2021-04-02 13:57:29 +0100

    Initial Commit

---
+++ sales/mytour.c
@@ -0,0 +1,8 @@
+#include "mytour.h"
+#include <stdio.h>
+
+
+void my_tour(const point cities[], int tour[], int ncities) {
+    printf("HELLO THERE!");
+    simple_find_tour(cities, tour, ncities);
+}
